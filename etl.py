d089c5ec5339d21a6abcb7ced1c65e89ef45bdbb
Adrián Vázquez <adri.forzaferrari@gmail.com>
2022-08-26 13:38:51 +0200

    Update etl.py

--- etl.py
+++ etl.py
@@ -4,18 +4,33 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    Load staging tables. These tables are used to later create the star schema
+    
+    :param cur: Database cursor
+    :param conn: Database connection
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """
+    Insert data into the star schema from the staging tables
+    
+    :param cur: Database cursor
+    :param conn: Database connection
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    Load the staging tables and then insert the data into the star schema
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
